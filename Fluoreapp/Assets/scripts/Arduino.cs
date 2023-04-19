3bbf206598c0af8cad0b16dec983c3089b7d7850
AkemiNaka <119260332+AkemiNaka@users.noreply.github.com>
2023-04-19 17:28:00 -0300

    arduino

---
+++ Fluoreapp/Assets/scripts/Arduino.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+ 
+
+public class Arduino : MonoBehaviour
+{
+
+
+
+    SerialPort serialPort;
+    string message;
+
+
+
+    void Start()
+    {
+        serialPort = new SerialPort("COM3", 9600);
+        serialPort.Open();
+    }
+
+
+
+    void Update()
+    {
+        if (serialPort != null && serialPort.IsOpen)
+        {
+            message = serialPort.ReadLine();
+            Debug.Log(message);
+        }
+
+
+
+    }
+}
+
